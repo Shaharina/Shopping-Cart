5c02124f22e581e97765d89eb202dbf875194cf7
Shaharina <S.mita90232@gmail.com>
2022-04-01 02:37:58 +0600

    confirm

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    //update casetotal
+    const casetotal = document.getElementById('case-total');
+    casetotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+
+})
